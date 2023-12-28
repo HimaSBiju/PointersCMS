eebc06fa9c6c92f16e03a8425a691fce09de2ced
sobhasoniya <sobhasoniya078@gmail.com>
2023-12-28 16:01:36 +0530

    shobha  addes model

---
+++ CMSDemo/CMSDemo/Model/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace CMSDemo.Model
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
